6c9f82aab13aa0cc8cfe14849ebd3c0c64db19d3
brendasustaita <157392809+brendasustaita@users.noreply.github.com>
2024-01-22 16:24:59 +0000

    Finish M1T2

---
+++ M1T2_Sustaita.cpp
@@ -0,0 +1,25 @@
+// CSC 134
+// M1T2
+// BRENDA SUSTAITA
+// 1/22/24
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    // Introduce yourself
+    cout << "Hello World!" <<endl;
+    cout << "My name is Brenda Sustaita" << "\n";
+    cout << "I am working towards my Associates in Science" << endl;
+    cout << "I am a server full time and when not at work or school, I am a full time soccer mom." << "\n";
+
+    //Ask a question
+    string name;
+    cout << "What's your name?";
+    cin >> name;
+
+    cout <<"Nice to meet you, " << name << endl;
+
+    return 0;
+}
+
