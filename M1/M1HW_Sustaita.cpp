299769dd798f09a5a0ad801542395da17e8f1e83
brendasustaita <157392809+brendasustaita@users.noreply.github.com>
2024-02-15 19:03:36 +0000

    completed M1HW

---
+++ M1/M1HW_Sustaita.cpp
@@ -0,0 +1,23 @@
+// CSC 134
+// M1HW
+// BRENDA SUSTAITA
+// 2/15/2024
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    string name = "Grease";
+    int year = 1978;
+    double box_office = 396.3;
+
+    // form statement
+    cout << "The movie " << name << " was made in " << year << "." << endl;
+    cout << "Grease made $" << box_office << " million USD, making it one of the highest-grossing musical ever in the US at that time." << endl;
+    cout << "The movie starred famous actors like John Travolta and Olivia Newton-John." << endl;
+    cout << "This movie is notoriously known for their sing-a-long songs and now popularly known quotes." << endl;
+    // quotes from movie
+    cout << "Frenchy is known for saying, \"The only man a girl can depend on is her daddy.\" after having a conversation with Sandy." << endl;
+    return 0;
+}
