3c12afec713176a85a9f6867544a3b71db9782c0
brendasustaita <157392809+brendasustaita@users.noreply.github.com>
2024-02-15 20:10:18 +0000

    added comments and small grammer adjustments

--- M2/M2T2_Sustaita.cpp
+++ M2/M2T2_Sustaita.cpp
@@ -9,26 +9,26 @@ using namespace std;
 
 int main ()
 {
-    // greet customer
+    // add variables
     string name;
     int numApples;
     double price;
     double total;
-
+    // greet customer
     cout << "Hello! Welcome to Sustaita Apple Farm!" << endl;
-
+    // ask customer for name
     cout << "What is your name:" << "\t\t\t"; 
     cin >> name;
-
+    // ask how many items they would like
     cout << "How many apples would you like to buy?" << "\t";
     cin >> numApples;
-
+    // calculate price
     cout << fixed << setprecision(2);
-    cout << "How much does each apple cost?" << "\t\t";
+    cout << "How much does each apple cost?" << "\t\t$";
     cin >> price;
 
     total = numApples * price;
-
+    // give total
     cout << "The total price for all of the apples is: $" << total << "." << endl;
     return 0;
 }
